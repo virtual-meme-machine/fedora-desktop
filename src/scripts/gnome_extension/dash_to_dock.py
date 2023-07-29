4cebc94783a4bd16b62bdfda3a523ad37ef9a3ec
Shane <118647167+ShaneMain@users.noreply.github.com>
2023-07-28 19:21:25 -0600

    Add ddterm, Dash to Dock, and NoAnnoyance Gnome Shell extensions

---
+++ src/scripts/gnome_extension/dash_to_dock.py
@@ -0,0 +1,11 @@
+from utils.dnf_utils import install_packages
+from utils.gnome_extension_utils import enable_extension
+
+
+def execute():
+    """
+    Moves the dash out of the overview transforming it in a dock
+    :return: None
+    """
+    install_packages(["gnome-shell-extension-dash-to-dock"])
+    enable_extension("dash-to-dock@micxgx.gmail.com")
