86d1bf3412086c0692859f496a4bb102995f1e6e
Shane Main <mainsha@protonmail.com>
2023-10-26 21:48:12 -0600

    Fixed doc link issues, used dnf utils to install dash to panel

--- src/scripts/gnome_extension/dash_to_panel.py
+++ src/scripts/gnome_extension/dash_to_panel.py
@@ -1,4 +1,5 @@
-from utils.gnome_extension_utils import enable_extension, install_remote_extension
+from utils.dnf_utils import install_packages
+from utils.gnome_extension_utils import enable_extension
 
 ID: str = "dash-to-panel@jderose9.github.com"
 
@@ -8,5 +9,5 @@ def execute():
     An icon taskbar for the Gnome Shell
     :return: None
     """
-    install_remote_extension(ID)
-    enable_extension(ID)
+    install_packages(["gnome-shell-extension-dash-to-panel"])
+    enable_extension("dash-to-panel@jderose9.github.com")
