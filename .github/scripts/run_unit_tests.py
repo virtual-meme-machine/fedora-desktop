8c21c6d6bda25b2b006375997792362f92ac3e89
virtual-meme-machine <mememaster720@protonmail.com>
2023-10-28 17:30:19 -0500

    - Fix GitHub Actions workflows

--- .github/scripts/run_unit_tests.py
+++ .github/scripts/run_unit_tests.py
@@ -6,5 +6,15 @@ import pytest
 __PACKAGE_ROOT: str = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", ".."))
 sys.path.append(os.path.join(__PACKAGE_ROOT, "src"))
 
+GITHUB_IGNORED_FILES: list[str] = [
+    "tst/data/test_OptionStore.py"  # gi imports cause crash: https://github.com/actions/runner-images/issues/4534
+]
+
 if __name__ == '__main__':
-    sys.exit(pytest.main())
+    args: list[str] = []
+    if "--github_ignore" in sys.argv:
+        for file in GITHUB_IGNORED_FILES:
+            print(f"Skipping tests in file: '{file}'")
+            args.append(f"--ignore={file}")
+
+    sys.exit(pytest.main(args=args))
