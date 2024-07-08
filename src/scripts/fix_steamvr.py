b55b605a68a41aa35930174dbc0fd5a6d15f85d1
virtual-meme-machine <mememaster720@protonmail.com>
2024-07-08 00:57:59 -0500

    - Add SteamVR .desktop file fixes to fix_steamvr.py script

--- src/scripts/fix_steamvr.py
+++ src/scripts/fix_steamvr.py
@@ -6,6 +6,39 @@ from utils.dnf_utils import is_package_installed
 STEAMVR_FOLDER: str = os.path.expanduser("~/.steam/steam/steamapps/common/SteamVR")
 STEAMVR_VRSETTINGS: str = os.path.expanduser("~/.steam/steam/config/steamvr.vrsettings")
 
+DESKTOP_FILE_DATA: dict = {
+    os.path.expanduser("~/.local/share/applications/valve-vrmonitor.desktop"): f"""[Desktop Entry]
+Name=SteamVR
+Comment=Application for the vrmonitor subcomponent of SteamVR
+Exec={os.path.join(STEAMVR_FOLDER, "bin/vrmonitor.sh")} %U
+Icon=application-x-vrmonitor
+Terminal=false
+Type=Application
+Categories=Game;
+MimeType=application/x-vrmonitor
+""",
+    os.path.expanduser("~/.local/share/applications/valve-URI-steamvr.desktop"): f"""[Desktop Entry]
+Name=URI-steamvr
+Comment=URI handler for steamvr://
+Exec={os.path.join(STEAMVR_FOLDER, "bin/linux64/vrurlhandler")} %U
+Terminal=false
+Type=Application
+Categories=Game;
+MimeType=x-scheme-handler/steamvr
+NoDisplay=true
+""",
+    os.path.expanduser("~/.local/share/applications/valve-URI-vrmonitor.desktop"): f"""[Desktop Entry]
+Name=URI-vrmonitor
+Comment=URI handler for vrmonitor://
+Exec={os.path.join(STEAMVR_FOLDER, "bin/vrmonitor.sh")} %U
+Terminal=false
+Type=Application
+Categories=Game;
+MimeType=x-scheme-handler/vrmonitor
+NoDisplay=true
+"""
+}
+
 
 def __fix_blank_windows():
     """
@@ -50,6 +83,32 @@ def __fix_blank_windows():
                 print(f"No saved DesktopUI windows found, skipping")
 
 
+def __fix_desktop_files():
+    """
+    Fixes SteamVR .desktop files that are missing or broken:
+    - https://github.com/ValveSoftware/SteamVR-for-Linux/issues/457
+    - https://github.com/ValveSoftware/SteamVR-for-Linux/issues/503
+    :return: None
+    """
+    for desktop_file in DESKTOP_FILE_DATA.keys():
+        print(f"Fixing SteamVR .desktop file: '{desktop_file}'")
+        file_contents = DESKTOP_FILE_DATA.get(desktop_file)
+
+        if os.path.isfile(desktop_file):
+            with open(desktop_file, "r") as file:
+                if file.read() == file_contents:
+                    print(f"SteamVR .desktop file is already fixed, skipping")
+                    continue
+
+        with open(desktop_file, "w") as file:
+            print(f"Writing updated SteamVR .desktop file")
+            file.seek(0)
+            file.write(file_contents)
+
+        print(f"Setting SteamVR .desktop file to read-only to prevent overwriting")
+        os.chmod(desktop_file, 0o444)
+
+
 def execute():
     """
     Applies various fixes to address known issues with SteamVR for Linux
@@ -64,3 +123,4 @@ def execute():
         return
 
     __fix_blank_windows()
+    __fix_desktop_files()
