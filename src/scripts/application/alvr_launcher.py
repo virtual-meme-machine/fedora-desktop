6a2eecca00c82529aaad014fbde9481a49fc7d38
virtual-meme-machine <mememaster720@protonmail.com>
2024-07-20 22:42:58 -0500

    - Fix ALVR Launcher install script

--- src/scripts/application/alvr_launcher.py
+++ src/scripts/application/alvr_launcher.py
@@ -8,6 +8,8 @@ DESKTOP_FILE: str = os.path.expanduser("~/.local/share/applications/ALVR-Launche
 DOWNLOAD_URL: str = "https://github.com/alvr-org/ALVR/releases/download/v20.9.1/alvr_launcher_linux.tar.gz"
 INSTALL_DIR: str = os.path.expanduser("~/.local/share/ALVR-Launcher")
 ALVR_LAUNCHER_EXEC: str = os.path.join(INSTALL_DIR, "ALVR Launcher")
+ALVR_ICON_PATH: str = os.path.join(INSTALL_DIR, "alvr.png")
+ALVR_ICON_URL: str = "https://avatars1.githubusercontent.com/u/74990209"
 
 
 def __install():
@@ -19,14 +21,17 @@ def __install():
         print("ALVR Launcher is already installed")
         return
 
+    print("Installing...")
+    if not os.path.exists(INSTALL_DIR):
+        os.makedirs(INSTALL_DIR)
+
     temp_dir = tempfile.mkdtemp()
     download_path = os.path.join(temp_dir, os.path.basename(DOWNLOAD_URL))
     download_file(url=DOWNLOAD_URL, output=download_path)
+    download_file(url=ALVR_ICON_URL, output=ALVR_ICON_PATH)
 
-    print("Installing...")
     shutil.unpack_archive(download_path, temp_dir)
-    os.makedirs(INSTALL_DIR)
-    shutil.move(os.path.join(temp_dir, "ALVR Launcher"), INSTALL_DIR)
+    shutil.move(os.path.join(temp_dir, "alvr_launcher_linux/ALVR Launcher"), ALVR_LAUNCHER_EXEC)
 
 
 def execute():
@@ -45,5 +50,6 @@ def execute():
                 "Terminal=false\n",
                 "Name=ALVR Launcher\n",
                 "Categories=Game;\n",
+                f"Icon={ALVR_ICON_PATH}\n",
                 f"Exec=\"{ALVR_LAUNCHER_EXEC}\"\n"
             ])
